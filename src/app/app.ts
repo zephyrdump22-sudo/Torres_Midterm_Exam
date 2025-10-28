b35a1f7d66e32149d09774dcd18fe138d22cb862
zephyrdump22-sudo <zephyr.dump22@gmail.com>
2025-10-28 17:24:25 +0800

    Deploy Angular app to GitHub Pages with 404.html redirect

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
